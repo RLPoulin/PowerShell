93973432616a8e1131cb9f967d0b7f7be11517f4
Robert Poulin <poulin@bioastratech.com>
2020-02-24 16:45:13 -0500

    Updated Start-Maintenance

--- Scripts/Start-Maintenance.ps1
+++ Scripts/Start-Maintenance.ps1
@@ -20,29 +20,40 @@ Param()
 
 Set-StrictMode -Version Latest
 
+$ExitCode = 0
+
+$Bioastra = $Env:COMPUTERNAME -eq "BIOASTRA5"
 $Documents = "$Home\Documents"
 $Scoop = (Get-Command scoop).Source
 $ScoopApps = "$Home\Scoop\Apps"
+$7ZipDLL = "$Documents\NppShell64.dll"
 
-$ToStop = @("AutoHotkeyU64", "KeePass")
-$ToStart = @(
-    "$Documents\AutoHotkey.ahk",
-    "$Home\scoop\apps\keepass\current\KeePass.exe"
-)
+$ToStop = @("AutoHotkeyU64")
+$ToStart = @("$HOME\OneDrive\Informatique\AutoHotkey.ahk")
 
-$FFSync = "$Env:ProgramFiles\FreeFileSync\FreeFileSync.exe"
-$BackupFiles = Get-Item @(
-    "\\server.bioastratech.com\Public\Time sheets\2019\Robert 2019.xlsm",
-    "\\server.bioastratech.com\Public\Time sheets\2020\Robert 2020.xlsm",
-    "$Documents\AutoHotkey.ahk",
-    "$Documents\Powershell"
-)
-$BackupFiles += Get-ChildItem $Home -Filter ".*" -File
-$BackupTarget = "$Documents\Backup"
-$MailBackup = Get-ChildItem "$Documents\eM Client\*.zip" |
-    Sort-Object -Property LastWriteTime |
-    Select-Object -Last 1
-$MailTarget = "$BackupTarget\eM Client Backup.zip"
+if ($Bioastra) {
+    $7ZipDLL = "$Documents\NppShell64.dll"
+    $ToStop += "KeePass"
+    $ToStart += @(
+        "$Documents\AutoHotkey.ahk",
+        "$Home\scoop\apps\keepass\current\KeePass.exe"
+    )
+
+    $FFSync = "$Env:ProgramFiles\FreeFileSync\FreeFileSync.exe"
+    $BackupFiles = Get-Item @(
+        "\\server.bioastratech.com\Public\Time sheets\2019\Robert 2019.xlsm",
+        "\\server.bioastratech.com\Public\Time sheets\2020\Robert 2020.xlsm",
+        "$Documents\AutoHotkey.ahk",
+        "$Documents\Powershell"
+    )
+    $BackupFiles += Get-ChildItem $Home -Filter ".*" -File
+    $BackupTarget = "$Documents\Backup"
+    $MailBackup = Get-ChildItem "$Documents\eM Client\*.zip" |
+        Sort-Object -Property LastWriteTime |
+        Select-Object -Last 1
+    $MailTarget = "$BackupTarget\eM Client Backup.zip"
+}
+
 
 #--------------------------------------[Functions]--------------------------------------
 
@@ -75,7 +86,7 @@ function Copy-UpdatedItem($Path, $Destination) {
     }
 }
 
-function Invoke-Pip() { & python -m pip $args }
+function Invoke-Pip() { & python -m pip $Args }
 
 
 #-------------------------------------[Execution]---------------------------------------
@@ -94,6 +105,7 @@ try {
 }
 catch {
     Write-ColoredOutput "`nError while cleaning Scoop." Red
+    $ExitCode += 1
 }
 Write-ColoredOutput "`nUpdating Scoop Packages.`n" Magenta
 try {
@@ -102,27 +114,29 @@ try {
 }
 catch {
     Write-ColoredOutput "`nError while updating software from Scoop." Red
+    $ExitCode += 1
 }
-Copy-UpdatedItem "$Documents\NppShell64.dll" "$ScoopApps\notepadplusplus\current\NppShell64.dll"
+Copy-UpdatedItem $7ZipDLL "$ScoopApps\notepadplusplus\current\NppShell64.dll"
 
 # Start processes
-$ToStart | ForEach-Object { . $_ }
-
+$ToStart | ForEach-Object { & $_ }
 
 # Conda
-Write-ColoredOutput "`nUpdating Conda Environments." Magenta
-(& "$Home\Miniconda3\Scripts\conda.exe" "shell.powershell" "hook") |
-    Out-String | Invoke-Expression
-Invoke-Conda clean --all --yes | Out-Null
-ForEach ($Env in (
-    Get-CondaEnvironment |
-        Select-Object -ExpandProperty Name |
-        Where-Object { -not $_.ToLower().Contains("pip") }
-)) {
-    Write-ColoredOutput "`nUpdating $($Env)..." Green
-    Invoke-Conda update --name $Env --all --yes
+if ($Bioastra) {
+    Write-ColoredOutput "`nUpdating Conda Environments." Magenta
+    (& "$Home\Miniconda3\Scripts\conda.exe" "shell.powershell" "hook") |
+        Out-String | Invoke-Expression
+    Invoke-Conda clean --all --yes | Out-Null
+    ForEach ($Env in (
+        Get-CondaEnvironment |
+            Select-Object -ExpandProperty Name |
+            Where-Object { -not $_.ToLower().Contains("pip") }
+    )) {
+        Write-ColoredOutput "`nUpdating $($Env)..." Green
+        Invoke-Conda update --name $Env --all --yes
+    }
+    Exit-CondaEnvironment -ErrorAction SilentlyContinue
 }
-Exit-CondaEnvironment -ErrorAction SilentlyContinue
 
 # Update Python packages
 Write-ColoredOutput "`nUpdating Pip Packages." Magenta
@@ -135,27 +149,29 @@ Write-ColoredOutput "`nUpdating Poetry." Magenta
 & poetry self update
 
 # Sync
-Write-ColoredOutput "`nSyncing Files." Magenta
-$BackupFiles | ForEach-Object { Copy-UpdatedItem $_ "$BackupTarget\$($_.Name)" }
-Copy-UpdatedItem $MailBackup $MailTarget
+if ($Bioastra) {
+    Write-ColoredOutput "`nSyncing Files." Magenta
+    $BackupFiles | ForEach-Object { Copy-UpdatedItem $_ "$BackupTarget\$($_.Name)" }
+    Copy-UpdatedItem $MailBackup $MailTarget
 
-Write-ColoredOutput "`nSyncing files from the server..." Green
-$Process = Start-Process $FFSync -ArgumentList `
-    "$Documents\Server-Local.ffs_batch" -Wait -PassThru
-
-if ($Process.ExitCode -eq 0) {
-    Write-ColoredOutput "`nSyncing files to Google Drive..." Green
+    Write-ColoredOutput "`nSyncing files from the server..." Green
     $Process = Start-Process $FFSync -ArgumentList `
-        "$Documents\Local-Google.ffs_batch" -Wait -PassThru
+        "$Documents\Server-Local.ffs_batch" -Wait -PassThru
 
-    if ($Process.ExitCode -ne 0) {
-        Write-ColoredOutput `
-            "`nError while syncing to Google Drive (Error $($Process.ExitCode))." Red
+    if ($Process.ExitCode -eq 0) {
+        Write-ColoredOutput "`nSyncing files to Google Drive..." Green
+        $Process = Start-Process $FFSync -ArgumentList `
+            "$Documents\Local-Google.ffs_batch" -Wait -PassThru
+
+        if ($Process.ExitCode -ne 0) {
+            Write-ColoredOutput `
+                "`nError while syncing to Google Drive (Error $($Process.ExitCode))." Red
+        }
     }
-}
-else {
-    Write-ColoredOutput `
-        "`nError while syncing from the server (Error $($Process.ExitCode))." Red
+    else {
+        Write-ColoredOutput `
+            "`nError while syncing from the server (Error $($Process.ExitCode))." Red
+    }
 }
 
 
