efa17ae5d359997aed0ec961da0711768b42333f
Robert Poulin <poulin@bioastratech.com>
2020-03-27 11:10:09 -0400

    Added Copy-Files cmdlet to MyFunctions.

--- Modules/MyFunctions/MyFunctions.psm1
+++ Modules/MyFunctions/MyFunctions.psm1
@@ -45,6 +45,45 @@ function Add-Path {
 }
 
 
+function Copy-File {
+    [CmdletBinding()]
+    [OutputType()]
+    [Alias()]
+
+    Param(
+        [Parameter(Position=1, Mandatory, ValueFromPipeline)]
+        [ValidateScript({Test-Path -Path $_})]
+        [string] $from,
+
+        [Parameter(Position=2, Mandatory)]
+        [string] $to
+    )
+
+    $ffile = [io.file]::OpenRead($from)
+    $tofile = [io.file]::OpenWrite($to)
+    Write-Progress -Activity "Copying file" -status "$from -> $to" -PercentComplete 0
+
+    try {
+        [byte[]]$buff = new-object byte[] 4096
+        [long]$total = [long]$count = 0
+        do {
+            $count = $ffile.Read($buff, 0, $buff.Length)
+            $tofile.Write($buff, 0, $count)
+            $total += $count
+            if ($total % 1mb -eq 0) {
+                Write-Progress -Activity "Copying file" -status "$from -> $to" `
+                   -PercentComplete ([int]($total/$ffile.Length* 100))
+            }
+        } while ($count -gt 0)
+    }
+    finally {
+        $ffile.Dispose()
+        $tofile.Dispose()
+        Write-Progress -Activity "Copying file" -Status "Ready" -Completed
+    }
+}
+
+
 function Get-Path {
     [CmdletBinding()]
     [OutputType([String[]])]
