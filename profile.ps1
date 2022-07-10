873734e3ab831999f6dfa90eaa07a3e77eba0f73
Robert Poulin <poulin.robert@gmail.com>
2022-07-10 12:18:56 -0400

    update environment variables and PSReadLine options in profile

--- profile.ps1
+++ profile.ps1
@@ -3,10 +3,10 @@
     My PowerShell profile.
 
 .NOTES
-    Version:        6.0.0
+    Version:        6.1.0
     Author:         Robert Poulin
     Creation Date:  2016-06-09
-    Updated:        2022-07-06
+    Updated:        2022-07-10
     License:        MIT
 
     TODO:
@@ -21,22 +21,25 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '')]
 [CmdletBinding()] Param()
 
-Import-Module posh-git -NoClobber
-Import-Module PSReadLine -NoClobber
-Import-Module PSWriteColor -NoClobber
-Import-Module Terminal-Icons -NoClobber
+Import-Module -Name posh-git -Global -NoClobber
+Import-Module -Name PSReadLine -Global -NoClobber
+Import-Module -Name PSWriteColor -Global -NoClobber
+Import-Module -Name Terminal-Icons -Global -NoClobber
 
-Import-Module MyFunctions -NoClobber -Force
-Import-Module DevFunctions -NoClobber -Force
+Import-Module -Name MyFunctions -Global -NoClobber -Force
+Import-Module -Name DevFunctions -Global -NoClobber -Force
 
 
 # Environment variables
 
 $PSFolder = $PSScriptRoot
 $Env:CodeFolder = "$Home\Code"
-$Env:BROWSER = 'msedge.exe'
-$Env:EDITOR = 'code.cmd'
+
+$Env:BAT_THEME = 'Visual Studio Dark+'
+$Env:BROWSER = 'msedge'
+$Env:EDITOR = 'code'
 $Env:POSH_GIT_ENABLED = 1
+$Env:POSH_THEMES_PATH = "$($Env:LOCALAPPDATA)\Programs\oh-my-posh\themes"
 $Env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
 
@@ -47,24 +50,31 @@ Set-Alias -Name ll -Value Get-ChildItem -Option AllScope -Scope Global
 Set-Alias -Name profile -Value $PSCommandPath -Option AllScope -Scope Global
 Set-Alias -Name uds -Value Update-Software -Option AllScope -Scope Global
 
-Remove-Alias ls -ErrorAction SilentlyContinue
-New-SimpleFunction Get-ChildItemWide -Value { Get-ChildItem | Format-Wide -AutoSize } -Alias ls -Force
-New-SimpleFunction Set-LocationToHome -Value { Enter-Location $HOME } -Alias '~' -Force
-New-SimpleFunction Set-LocationToParent -Value { Enter-Location '..' } -Alias '..' -Force
+Remove-Alias -Name ls -ErrorAction SilentlyContinue
+New-SimpleFunction -Name Get-ChildItemWide -Value { Get-ChildItem | Format-Wide -AutoSize } -Alias ls -Force
+New-SimpleFunction -Name Set-LocationToHome -Value { Enter-Location $HOME } -Alias '~' -Force
+New-SimpleFunction -Name Set-LocationToParent -Value { Enter-Location '..' } -Alias '..' -Force
 
-New-ProxyCommand 'Get-HelpOnline' 'Get-Help' -Default @{ 'Online' = $True } -Alias 'gho' -Force
-New-ProxyCommand 'Get-HelpFull' 'Get-Help' -Default @{ 'Full' = $True } -Alias 'ghf' -Force
-New-ProxyCommand 'Get-HiddenChildItem' 'Get-ChildItem' -Default @{ 'Force' = $True } -Alias 'la' -Force
-New-ProxyCommand 'Remove-Directory' 'Remove-Item' -Default @{ 'Recurse' = $True } -Alias 'rd' -Force
+New-ProxyCommand -Name 'Get-HelpOnline' 'Get-Help' -Default @{ 'Online' = $True } -Alias 'gho' -Force
+New-ProxyCommand -Name 'Get-HelpFull' 'Get-Help' -Default @{ 'Full' = $True } -Alias 'ghf' -Force
+New-ProxyCommand -Name 'Get-HiddenChildItem' 'Get-ChildItem' -Default @{ 'Force' = $True } -Alias 'la' -Force
+New-ProxyCommand -Name 'Remove-Directory' 'Remove-Item' -Default @{ 'Recurse' = $True } -Alias 'rd' -Force
 
 
 # Set Prompt
 
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView -WarningAction SilentlyContinue
+$PSReadLineOptions = @{
+    BellStyle = 'Visual'
+    PredictionSource = 'HistoryAndPlugin'
+    PredictionViewStyle = 'ListView'
+}
+Set-PSReadLineOption @PSReadLineOptions
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord 'Alt+UpArrow' -Function YankLastArg
+Set-PSReadLineKeyHandler -Chord 'Alt+RightArrow' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Chord 'Shift+Tab' -Function Complete
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 oh-my-posh init pwsh --config "$PSFolder\prompt-pure.omp.yaml" | Invoke-Expression
 
 
@@ -101,9 +111,9 @@ function Show-Greeting {
         LinesBefore = 1
         LinesAfter = 1
     }
-    Write-Color $version @versionFormat
-    Write-Color "Hi $($Env:USERNAME)!" -Color Magenta -LinesAfter 1
-    Remove-Item function:Show-Greeting
+    Write-Color -Text $version @versionFormat
+    Write-Color -Text "Hi $($Env:USERNAME)!" -Color Magenta -LinesAfter 1
+    Remove-Item -Path function:Show-Greeting
 }
 
 Show-Greeting
