97e28bf3e69b1cb17fd7c87ad3cad7e1548f25b3
Robert Poulin <poulin.robert@gmail.com>
2024-04-29 15:05:04 -0400

    Update-Software: major improvements

--- Scripts/Update-Software.ps1
+++ Scripts/Update-Software.ps1
@@ -3,7 +3,7 @@
     Update most installed software.
 
 .DESCRIPTION
-    Updates all software installed with winget, scoop, and modules from NuGet.
+    Updates software with WinGet, Scoop, Pipx, PowerShell modules, and Windows Update.
 
 .EXAMPLE
     Update-Software -Shutdown
@@ -15,85 +15,131 @@
     None
 
 .NOTES
-    Version:        1.2.0
+    Version:        2.0.0
     Author:         Robert Poulin
     Creation Date:  2022-07-06
-    Updated:        2023-10-14
+    Updated:        2024-04-15
     License:        MIT
-
-    TODO:
-        - Check https://github.com/Romanitho/Winget-AutoUpdate
 #>
 
-#Requires -Version 7.2
+#Requires -Version 7.4
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPositionalParameters', '', Justification = 'Messes with scoop.')]
 [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
 Param (
     # If true, will shutdown the computer 1 minute after the updates.
-    [Parameter()] [Switch] $Shutdown
+    [Parameter()] [Switch] $Shutdown,
+
+    # If true, will force all updates.
+    [Parameter()] [Switch] $Force
 )
 
+begin {
+    Set-StrictMode -Version Latest
 
-Set-StrictMode -Version Latest
+    $importArgs = @{
+        NoClobber = $True
+        Force = $Force
+        ErrorAction = 'Stop'
+        Verbose = $False
+    }
+    Import-Module -Name MyFunctions @importArgs
+    Import-Module -Name gsudoModule @importArgs
 
-Import-Module -Name MyFunctions -NoClobber -ErrorAction Stop -Verbose:$False
-Import-Module -Name PSWindowsUpdate -NoClobber -ErrorAction Stop -Verbose:$False
+    $headerStyle = @{ Style = 'Header'; Time = $True }
 
-
-if (!(Test-Administrator)) {
-    if (Test-Command gsudo) {
-        Write-Message -Message 'This script requires local admin privileges. Elevating...' -Style 'Warning'
-        gsudo "$($MyInvocation.MyCommand.Source)" $args
-        if ($LastExitCode -eq 999 ) {
-            throw 'Failed to elevate.'
+    if (!(Test-IsProcessElevated)) {
+        Write-Message -Message 'This script requires administrator privileges. Elevating...' -Style Warning
+        gsudo cache on --duration 1:00 *> $Null
+        if (Test-IsGsudoCacheAvailable) {
+            Write-Message 'Gsudo cache is available.' -Style Debug
         }
-        exit $LastExitCode
+        else {
+            Write-Message 'Gsudo cache is not available.' -Style Error
+            Exit
+        }
     }
-    else {
-        throw 'This script requires local admin privileges.'
+}
+
+process {
+    if ($PSCmdlet.ShouldProcess('WinGet')) {
+        Write-Message -Message 'Updating Winget applications...' @headerStyle
+        $wingetProcessArgs = @{
+            FilePath = (Get-Command -Name 'winget').Path
+            ArgumentList = 'upgrade', '--all', '--silent', '--accept-package-agreements'
+            NoNewWindow = $True
+            Wait = $True
+        }
+        Write-Message -Message "Starting '$($wingetProcessArgs.FilePath) $($wingetProcessArgs.ArgumentList)'." -Style Debug
+        gsudo { param($a); Start-Process @a } -args $wingetProcessArgs
+    }
+
+    if ((Test-Command 'scoop') -and $PSCmdlet.ShouldProcess('Scoop')) {
+        Write-Message -Message 'Updating Scoop...' @headerStyle
+        Write-Message -Message "Starting 'scoop update'." -Style Debug
+        . scoop update *> $Null
+        Write-Message -Message "Starting 'scoop cleanup --global --all'." -Style Debug
+        gsudo { . scoop cleanup --global --all *> $Null }
+        Write-Message -Message "Starting 'scoop cleanup --all'." -Style Debug
+        . scoop cleanup --all *> $Null
+
+        Write-Message -Message 'Updating Scoop applications...' @headerStyle
+        Write-Message -Message "Starting 'scoop update --global --all'." -Style Debug
+        gsudo { . scoop update --global --all }
+        Write-Message -Message "Starting 'scoop update --all'." -Style Debug
+        . scoop update --all
+    }
+
+    if ((Test-Command 'pipx') -and $PSCmdlet.ShouldProcess('Pipx')) {
+        Write-Message -Message 'Updating Python modules...' @headerStyle
+        Write-Message -Message "Starting 'pip install --upgrade pip'." -Style Debug
+        . py -m pip install --upgrade --user --upgrade-strategy eager --quiet pip
+        Write-Message -Message "Starting 'pip install --upgrade pipx'." -Style Debug
+        . py -m pip install --upgrade --user --upgrade-strategy eager --quiet pipx
+
+        Write-Message -Message 'Updating Pipx packages...' @headerStyle
+        Write-Message -Message "Starting 'pipx upgrade-all'." -Style Debug
+        . pipx upgrade-all --quiet
+    }
+
+    if ($PSCmdlet.ShouldProcess('PowerShell Modules')) {
+        Write-Message -Message 'Updating Powershell modules...' @headerStyle
+        $updateModuleArgs = @{
+            AcceptLicense = $True
+            Confirm = $False
+            Force = [bool] $Force
+        }
+        Write-Message -Message "Starting 'Update-Module -Scope CurrentUser'." -Style Debug
+        Update-Module -Scope CurrentUser @updateModuleArgs
+        Write-Message -Message "Starting 'Update-Module -Scope AllUsers'." -Style Debug
+        gsudo { param($a); Update-Module -Scope AllUsers @a } -args $updateModuleArgs
+    }
+
+    if ($PSCmdlet.ShouldProcess('Windows Update')) {
+        Write-Message -Message 'Running Windows Update...' @headerStyle
+        Import-Module -Name PSWindowsUpdate @importArgs
+        $windowsUpdateArgs = @{
+            Install = $True
+            AcceptAll = $True
+            RecurseCycle = 2
+            AutoReboot = [bool] $Shutdown
+            IgnoreReboot = [bool] !($Shutdown)
+            Confirm = $False
+        }
+        gsudo { param($a); Get-WindowsUpdate @a } -args $windowsUpdateArgs
+    }
+
+    Write-Message -Message 'Updates Completed!' @headerStyle
+}
+
+end {
+    if ($Shutdown) {
+        Write-Message -Message 'Shutting down in 10 minutes!' -Style Warning
+        Start-Shutdown -Minutes 10
     }
 }
 
-if ($PSCmdlet.ShouldProcess('Winget', 'upgrade --all')) {
-    Write-Message -Message 'Updating Winget applications...' -Style 'Header' -Time
-    winget upgrade --all --source Winget --silent --accept-package-agreements
+clean {
+    gsudo cache off *> $Null
 }
 
-if ((Test-Command 'scoop') -and $PSCmdlet.ShouldProcess('Scoop', 'update *')) {
-    Write-Message -Message 'Updating Scoop...' -Style 'Header' -Time
-    scoop update *> $Null
-    scoop cleanup * *> $Null
-    Write-Message -Message 'Updating Scoop applications...' -Style 'Header' -Time
-    scoop update *
-}
-
-if ((Test-Command 'pipx') -and $PSCmdlet.ShouldProcess('Pipx', 'upgrade-all')) {
-    Write-Message -Message 'Updating Pipx packages...' -Style 'Header' -Time
-    pipx upgrade-all
-}
-
-if ($PSCmdlet.ShouldProcess('PowerShell modules', 'update')) {
-    Write-Message -Message 'Updating Powershell modules...' -Style 'Header' -Time
-    Update-Module -Scope AllUsers -AcceptLicense -Confirm:$False
-    Update-Module -Scope CurrentUser -AcceptLicense -Confirm:$False
-}
-
-if ($PSCmdlet.ShouldProcess('Windows Update', 'update')) {
-    $updateArgs = @{
-        Install = $True
-        AcceptAll = $True
-        RecurseCycle = 2
-        AutoReboot = $Shutdown
-        IgnoreReboot = !($Shutdown)
-        Verbose = $True
-        Confirm = $False
-    }
-    Write-Message -Message 'Running Windows Update...' -Style 'Header' -Time
-    Get-WindowsUpdate @updateArgs
-}
-
-Write-Message -Message 'Done!' -Style 'Header'
-
-if ($Shutdown) {
-    Start-Shutdown -Minutes 10
-}
