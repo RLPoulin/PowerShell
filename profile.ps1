eb0a5bd62e663bd80c5590ff0db0394e86e1f36e
Robert Poulin <poulin.robert@gmail.com>
2024-05-03 18:20:42 -0400

    improve profile

--- profile.ps1
+++ profile.ps1
@@ -3,10 +3,10 @@
     My PowerShell profile.
 
 .NOTES
-    Version:        7.2.0
+    Version:        7.3.0
     Author:         Robert Poulin
     Creation Date:  2016-06-09
-    Updated:        2024-04-15
+    Updated:        2024-04-30
     License:        MIT
 
 #>
@@ -14,14 +14,17 @@
 #Requires -Version 7.4
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Necessary to setup of some tools.')]
-[CmdletBinding()] Param(
-    [Parameter()] [Switch] $Force
-)
-
-if (!([Environment]::UserInteractive) -or ($Host.Name -eq 'ConsoleHost' -and $Host.Version -lt '7.4')) { exit }
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '', Justification = 'Setting globals in profile is appropriate.')]
+[CmdletBinding()] Param([Parameter()] [Switch] $Force)
 
 Set-StrictMode -Version Latest
 
+if (!([Environment]::UserInteractive) -or ($Host.Name -eq 'ConsoleHost' -and $Host.Version -lt '7.4')) { exit }
+
+
+# → Import modules
+
+Import-Module -Name gsudoModule -NoClobber
 Import-Module -Name PSWriteColor -NoClobber
 Import-Module -Name Terminal-Icons -NoClobber
 
@@ -29,77 +32,92 @@ Import-Module -Name MyFunctions -Force:$Force
 Import-Module -Name DevFunctions -Force:$Force
 
 
+# → Set global and environment variables.
+
 Write-Message -Message 'Setting up environment variables...' -Style Verbose -Color Cyan
 
-$PSFolder = $PSScriptRoot
-$PSProfile = $PSCommandPath
-$Env:CodeFolder = (Resolve-Path "$HOME\Code" -ErrorAction Ignore)?.Path
+$Global:CodeFolder = (Resolve-Path "$HOME\Code" -ErrorAction Ignore)?.Path
+$Global:PSFolder = $PSScriptRoot
+$Global:MyProfile = $PSCommandPath
 
 $Env:BAT_THEME = 'Visual Studio Dark+'
 $Env:EDITOR = 'code'
-$Env:POSH_GIT_ENABLED = 1
-$Env:POSH_THEMES_PATH = (Resolve-Path "$($Env:LOCALAPPDATA)\Programs\oh-my-posh\themes" -ErrorAction Ignore)?.Path
-$Env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
 
-Write-Message -Message 'Setting up aliases and proxy functions...' -Style Verbose -Color Cyan
+# → Define proxy functions and aliases.
 
-Set-Alias -Name 'll' -Value Get-ChildItem -Force:$Force
-New-SimpleFunction -Name Get-ChildItemWide -Value { Get-ChildItem @args | Format-Wide -AutoSize } -Alias 'ls' -Force:$Force
+Write-Message -Message 'Defining proxy functions and aliases...' -Style Verbose -Color Cyan
+
+New-ProxyCommand -Name Get-HelpFull -Value Get-Help -Default @{ 'Full' = $True } -Alias 'ghf' -Force:$Force
+New-ProxyCommand -Name Get-HelpOnline -Value Get-Help -Default @{ 'Online' = $True } -Alias 'gho' -Force:$Force
 New-ProxyCommand -Name Get-HiddenChildItem -Value Get-ChildItem -Default @{ 'Force' = $True } -Alias 'la' -Force:$Force
-
+New-ProxyCommand -Name Remove-Directory -Value Remove-Item -Default @{ 'Recurse' = $True } -Alias 'rd' -Force:$Force
+New-SimpleFunction -Name Get-ChildItemWide -Value { Get-ChildItem @args | Format-Wide -AutoSize } -Alias 'ls' -Force:$Force
 New-SimpleFunction -Name Set-LocationToHome -Value { Update-Location $HOME } -Alias '~' -Force:$Force
 New-SimpleFunction -Name Set-LocationToParent -Value { Update-Location '..' } -Alias '..' -Force:$Force
-New-ProxyCommand -Name Remove-Directory -Value Remove-Item -Default @{ 'Recurse' = $True } -Alias 'rd' -Force:$Force
+Set-Alias -Value Get-ChildItem -Name 'll' -Force:$Force
+Set-Alias -Value Get-Help -Name 'gh' -Force:$Force
 
-Set-Alias -Name 'gh' -Value Get-Help -Force:$Force
-New-ProxyCommand -Name Get-HelpOnline -Value Get-Help -Default @{ 'Online' = $True } -Alias 'gho' -Force:$Force
-New-ProxyCommand -Name Get-HelpFull -Value Get-Help -Default @{ 'Full' = $True } -Alias 'ghf' -Force:$Force
 
+# → Define profile-related functions.
 
 Write-Message -Message 'Defining profile-related functions...' -Style Verbose -Color Cyan
 
 function Edit-Profile {
     [CmdletBinding()] [Alias('edp')] param()
 
-    Push-Location -Path $PSFolder -StackName Profile
-    & $Env:Editor "$PSProfile"
-    Pop-Location -StackName Profile
+    Push-Location -Path $PSFolder
+    if ($Env:Editor -eq 'code') {
+        & code .
+    }
+    else {
+        & $Env:Editor "$MyProfile"
+    }
+    Pop-Location
 }
 
 function Set-Profile {
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'None')] [Alias('profile')] param()
 
-    if ($PSCmdlet.ShouldProcess($PSProfile)) {
-        . $PSProfile -Force
+    if ($PSCmdlet.ShouldProcess($MyProfile)) {
+        . $MyProfile -Force
     }
 }
 
 
-Write-Message -Message 'Setting up prompt...' -Style Verbose -Color Cyan
+# → Set prompt options.
+
+Write-Message -Message 'Setting up prompt options...' -Style Verbose -Color Cyan
+
+$Env:POSH_GIT_ENABLED = 1
+$Env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
 oh-my-posh init pwsh --config (Join-Path -Path $PSFolder -ChildPath prompt-pure.omp.yaml) | Invoke-Expression
-oh-my-posh completion powershell | Out-String | Invoke-Expression
 
-$PSReadLineOptions = @{
+$ProfileReadlineOptions = @{
     BellStyle = 'Visual'
     HistorySearchCursorMovesToEnd = $True
     PredictionSource = 'HistoryAndPlugin'
     PredictionViewStyle = 'ListView'
 }
-Set-PSReadLineOption @PSReadLineOptions
+Set-PSReadLineOption @ProfileReadlineOptions
 
-@(
+$ProfileReadlineKeys = @(
     @{Chord = 'UpArrow'; Function = 'HistorySearchBackward' }
     @{Chord = 'DownArrow'; Function = 'HistorySearchForward' }
     @{Chord = 'Alt+UpArrow'; Function = 'YankLastArg' }
     @{Chord = 'Alt+RightArrow'; Function = 'ForwardWord' }
     @{Chord = 'Tab'; Function = 'MenuComplete' }
     @{Chord = 'Shift+Tab'; Function = 'Complete' }
-) | ForEach-Object { Set-PSReadLineKeyHandler @_ }
+)
+$ProfileReadlineKeys | ForEach-Object { Set-PSReadLineKeyHandler @_ }
 
 
-Write-Message -Message 'Setting up argument completers...' -Style Verbose -Color Cyan
+# → Register argument completers.
+
+Write-Message -Message 'Registering argument completers...' -Style Verbose -Color Cyan
+
+oh-my-posh completion powershell | Out-String | Invoke-Expression
 
 if (Test-Command rustup) {
     rustup completions powershell | Out-String | Invoke-Expression
@@ -118,6 +136,8 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     }
 }
 
+
+# → Show greeting.
 
 Write-Message -Message 'Showing greeting...' -Style Verbose -Color Cyan
 
@@ -141,6 +161,7 @@ function Show-Greeting {
         LinesBefore = 1
         LinesAfter = 1
     }
+
     Write-Color @versionArgs
     Write-Color @nameArgs
     if (Test-Administrator) { Write-Color @adminWarningArgs }
